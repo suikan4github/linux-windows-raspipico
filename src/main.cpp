23d39b4761ca4b91db7f735d5f981303a993666f
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2024-10-23 12:47:17 +0900

    Program working. Initial stage.

--- src/main.cpp
+++ src/main.cpp
@@ -9,41 +9,21 @@
  *
  */
 #include <iostream>
-#include <thread>
+
 #include "calc.hpp"
-
-/**
- * @brief Calculate the square root of 1 to the given num.
- *
- * @param num Max value to calc the square root
- */
-void CalcSqrt(int num)
-{
-    Calc calc;
-
-    // count from 1 to num
-    for (int i = 1; i <= num; i++)
-        std::cout << "[CalcSqrt] The square root of " << i << " is " << calc.sqrt(i) << std::endl;
-}
 
 /**
  * @brief main function
  *
  * @return int Always 0
  */
-int main()
-{
-    std::cout << "[main] Thread start." << std::endl;
+int main() {
+  Calc calc;
 
-    // run the CalcSaqrt() as thread. This is for the test of the std::thread functionality in the each platform
-    std::thread t(CalcSqrt, 10);
+  // count from 1 to num
+  for (int i = 1; i <= 10; i++)
+    std::cout << "[CalcSqrt] The square root of " << i << " is " << calc.sqrt(i)
+              << std::endl;
 
-    std::cout << "[main] Waiting for the end of thread." << std::endl;
-
-    // wait for the end of thread execution.
-    t.join();
-
-    std::cout << "[main] Thread terminated." << std::endl;
-
-    return 0;
+  return 0;
 }
